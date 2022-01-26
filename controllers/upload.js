4134eee4abd47456191a75b27e4f434b4c343195
christian <habinezac15@gmail.com>
2022-01-26 13:38:41 +0200

    adding start script in package.json for allowing heroku to load

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,39 +1,3 @@
-/* 
-const formidable = require('formidable');
-const detect = require('detect-file-type');
-const fs = require('fs');
-const path = require('path');
-const multer = require('multer');
-
-// uploading image
-const upload = () => {
-  new formidable.IncomingForm().parse(req, (err, fields, files) => {
-    // detecting file extension
-    detect.fromFile(files.image.filepath, (err, result) => {
-      // setting allowed extensions
-      console.log(files.image);
-      const pictureName = `${files.image.originalFilename}${Date.now()}.${
-        result.ext
-      }`;
-      const allowedImageType = ['jpg', 'jpeg', 'png'];
-      if (!allowedImageType.includes(result.ext)) {
-        return res.send('please choose file with image extension');
-      }
-      const oldPath = files.image.filepath;
-      const newPath = path.join(__dirname, '..', 'images', pictureName);
-      fs.rename(oldPath, newPath, (err) => {
-        if (err) {
-          return res.send('cannot move the file');
-        }
-        const imgURL = `http://localhost:8080/file/${files.image.originalFilename}`;
-        res.send(imgURL);
-      });
-    });
-  });
-};
-module.exports = multer({ storage: upload });
-*/
-
 const multer = require('multer');
 
 const storage = multer.diskStorage({
