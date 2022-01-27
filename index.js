7cf68db26c340ec512e3e497d36348056550ff62
christian <habinezac15@gmail.com>
2022-01-27 09:25:00 +0200

    Acces-Controlls added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose.connect(dbURI, () => {
 // middlewares
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header({
     'Access-Control-Allow-Headers':
       'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
