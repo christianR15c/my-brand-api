bd96fa8f096060113080ab777af012e4d94063ba
christian <habinezac15@gmail.com>
2022-01-27 09:14:58 +0200

    Acces-Controlls added

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ mongoose.connect(dbURI, () => {
 
 // middlewares
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.headers({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  });
   next();
 });
 app.use('/images', express.static('images'));
